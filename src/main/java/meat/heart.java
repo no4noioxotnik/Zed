e2cc9047c98aa3af0da83e6fa63f3c8fa27fd34e
ragimov_ra <rustam.ragimov@open.ru>
2017-10-10 18:41:48 +0300

    1

--- src/main/java/meat/heart.java
+++ src/main/java/meat/heart.java
@@ -1,6 +1,11 @@
 package meat;
 
 import java.io.File;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+
+import static java.awt.SystemColor.text;
+import static sun.plugin.ClassLoaderInfo.reset;
 
 
 public class heart {
@@ -8,44 +13,9 @@ public class heart {
     static boolean flag;
 
     public static void main(String[] args) {
-        func("c:","Test");
-        f(null);
-    }public static void f(NullPointerException e) {
-        try {
-            func("C:", "readme");
-            throw e;
-        } catch (NullPointerException npe) {
-            //f(npe);
-        }
-    }
 
-    static void func(String path, String find) {
-        File f = new File(path);
-        String[] list = f.list();
-        for (String file : list) {
-                if (find.equals(file)) {
-                flag=true;
-                System.out.println(path + "\\" + file + " !!!!!!!!!!!!!!!!!!");
-                return;
-            }
-            if (!path.endsWith("\\")) {
-                path += "\\";
-            }
-            File tempfile = new File(path + file);
-            System.out.println(path + file);
-            if (!file.equals(".") && !file.equals("..")) {
-                if (tempfile.isDirectory()) {
-                    //path += file;
-                    func(path + file, find);
-                    if(flag) return;
-                }
-            }
-        }
-        if (list == null) {
-            return;
-    }
 
-}
+        }
 }
 
 
